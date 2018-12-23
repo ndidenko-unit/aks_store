016ea7c5eafe3721a512433504a0827b982484ed
ndidenko <5773480@gmail.com>
2018-12-23 21:05:32 +0200

    added views for items

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,5 @@ class Item < ApplicationRecord
   belongs_to :store
 
   validates :name, :purchase, :retail, :store_id, :status_id, presence: true
-end
+  validates :purchase, :retail, numericality: true
+ end
